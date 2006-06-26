83bb070808b776ca0c1e8d8692143c80b581e10a
cedricdourneau <cedric.dourneau@gmail.com>
2006-06-26 14:49:02 +0000

    Rendu les *.c presque lisibles.

--- coll.c
+++ coll.c
@@ -30,123 +30,133 @@
 
 
 
-double dabs(double x)
+inline double dabs(double x)
 {
-  return ((x>0) ? x : (-1)*x);
+	return ((x>0) ? x : (-1)*x);
 }
 
-double max(double x,double y)
+inline double max(double x,double y)
 {
-  return ((x>y) ? x : y);
+	return ((x>y) ? x : y);
 }
 
-double min(double x,double y)
+inline double min(double x,double y)
 {
 	return ((x<y) ? x : y);
 }
 
-int truemod(int x,int n)
+inline int truemod(int x,int n)
 {
-  int y=x;
-  while (y<0)
-    y += n;
-  return (y%n);
+	int y=x%n;
+	if (y<0)
+		y += n;
+	return y;
 } 
 
 double SINUS_LIST[3600];
 double COSINUS_LIST[3600];
 
+#define array_elem(type) (*(double *)(array->data + ARRAY_##type*array->strides[0] + i*array->strides[1]))
+
 static PyObject *coll(PyObject *self, PyObject *args)
 {
-  double x,y,rat_x,rat_y,t_x,t_y,b_x,b_y,b_d,b_s;
-  int nb_players,size,signe_x,signe_y,i,result=-1;
-  PyArrayObject *array;
-  PyArg_ParseTuple(args,"O!iddi",&PyArray_Type,&array,&size,&x,&y,&nb_players);
-  
-  for (i=0;i<size;i++)
-    {
-      b_x = *(double *)(array->data + ARRAY_X*array->strides[0] + i*array->strides[1]);
-      b_y = *(double *)(array->data + ARRAY_Y*array->strides[0] + i*array->strides[1]);
-      
-      if ((int)*(double *)(array->data + ARRAY_STATE*array->strides[0] + i*array->strides[1]) == ARRAY_STATE_ML)
+	double x,y;
+	double rat_x,rat_y;
+	double t_x,t_y;
+	double b_x,b_y;
+	double b_d,b_s;
+
+	int nb_players;
+	int size;
+	int signe_x,signe_y;
+	int i;
+	int result=-1;
+
+	PyArrayObject *array;
+	PyArg_ParseTuple(args,"O!iddi",&PyArray_Type,&array,&size,&x,&y,&nb_players);
+
+	for (i=0;i<size;i++)
+	{
+		b_x = array_elem(X);
+		b_y = array_elem(Y);
+
+		if ((int) array_elem(STATE) == ARRAY_STATE_ML)
 			if ((dabs(b_x - x) < RADIUS) && (dabs(b_y - y)< RADIUS))
-	  			return (Py_BuildValue("i",i));
-      
-      if ((int)*(double *)(array->data + ARRAY_STATE*array->strides[0] + i*array->strides[1]) >= ARRAY_STATE_DANG)
-			{	      
-	  			if (*(double *)(array->data + ARRAY_UNTIL*array->strides[0] + i*array->strides[1]) > 0 && (int)*(double *)(array->data + ARRAY_STATE*array->strides[0] + i*array->strides[1]) == ARRAY_STATE_DANG) 
-	    			(*(double *)(array->data + ARRAY_UNTIL*array->strides[0] + i*array->strides[1]))--;
-	  			else
-	    			{
-	      b_d = *(double *)(array->data + ARRAY_DIRECTION*array->strides[0] + i*array->strides[1]);
-	      b_s = *(double *)(array->data + ARRAY_SPEED*array->strides[0] + i*array->strides[1]);
-	      
-	      
-	      
-	      (*(double *)(array->data + ARRAY_STATE*array->strides[0] + i*array->strides[1])) = (double)(ARRAY_STATE_NO_DANG);
-	      if (dabs(b_x - x) > RADIUS)
+				return (Py_BuildValue("i",i));
+
+		if ((int) array_elem(STATE) >= ARRAY_STATE_DANG)
 		{
-		  signe_x = (b_x > x) + (b_x >= x ) - 1;
-		  rat_x = SINUS_LIST[truemod((int)(10*b_d),3600)]*b_s - (double)(signe_x * PLAYER_SPEED);
-		  if (rat_x != 0)
-		    {
-		      t_x = (signe_x * RADIUS - b_x + x) / rat_x;
-		      if (t_x >= 0 && (-1)*UNIT_WIDTH < (x + signe_x * t_x * PLAYER_SPEED) && (x + signe_x * t_x * PLAYER_SPEED) < UNIT_WIDTH)
+			if (array_elem(UNTIL) > 0 && (int) array_elem(STATE) == ARRAY_STATE_DANG) 
+				array_elem(UNTIL)--;
+			else
 			{
-			  if (dabs(b_y - y) > RADIUS)
-			    {
-			      signe_y = (b_y > y) + (b_y >= y) - 1;
-			      rat_y = (-1)*COSINUS_LIST[truemod((int)(10*b_d),3600)]*b_s - signe_y * PLAYER_SPEED;
-			      if (rat_y != 0)
+				b_d = array_elem(DIRECTION);
+				b_s = array_elem(SPEED);
+
+				array_elem(STATE) = (double)(ARRAY_STATE_NO_DANG);
+				if (dabs(b_x - x) > RADIUS)
 				{
-				  t_y = (signe_y * RADIUS - b_y + y) / rat_y;
-				  if (t_y >= 0)
-				    {
-					 	if (((int)*(double *)(array->data + ARRAY_STATE*array->strides[0] + i*array->strides[1])) >= (ARRAY_STATE_DANG + nb_players - 1)) 
-				      	(*(double *)(array->data + ARRAY_STATE*array->strides[0] + i*array->strides[1])) = (double)(ARRAY_STATE_DANG);
-						else
-				      	(*(double *)(array->data + ARRAY_STATE*array->strides[0] + i*array->strides[1]))++;
-							
-				      (*(double *)(array->data + ARRAY_UNTIL*array->strides[0] + i*array->strides[1])) = min((max (t_x, t_y)),  (*(double *)(array->data + ARRAY_UNTIL*array->strides[0] + i*array->strides[1])) );
-				    }
+					signe_x = (b_x > x) + (b_x >= x ) - 1;
+					rat_x = SINUS_LIST[truemod((int)(10*b_d),3600)]*b_s - (double)(signe_x * PLAYER_SPEED);
+					if (rat_x != 0)
+					{
+						t_x = (signe_x * RADIUS - b_x + x) / rat_x;
+						if (t_x >= 0 && (-1)*UNIT_WIDTH < (x + signe_x * t_x * PLAYER_SPEED) && (x + signe_x * t_x * PLAYER_SPEED) < UNIT_WIDTH)
+						{
+							if (dabs(b_y - y) > RADIUS)
+							{
+								signe_y = (b_y > y) + (b_y >= y) - 1;
+								rat_y = (-1)*COSINUS_LIST[truemod((int)(10*b_d),3600)]*b_s - signe_y * PLAYER_SPEED;
+								if (rat_y != 0)
+								{
+									t_y = (signe_y * RADIUS - b_y + y) / rat_y;
+									if (t_y >= 0)
+									{
+										if ((int) array_elem(STATE) >= ARRAY_STATE_DANG + nb_players - 1) 
+											array_elem(STATE) = (double)(ARRAY_STATE_DANG);
+										else
+											array_elem(STATE)++;
+
+										array_elem(UNTIL) = min(max(t_x, t_y), array_elem(UNTIL));
+									}
+								}
+							}
+							else
+							{
+								if ((int)array_elem(STATE) >= ARRAY_STATE_DANG + nb_players - 1)
+									array_elem(STATE) = (double)(ARRAY_STATE_DANG);
+								else
+									array_elem(STATE)++;
+
+								array_elem(UNTIL) = min(t_x, array_elem(UNTIL));
+							}
+						}
+					}
 				}
-			    }
-			  else
-			    {
-					 	if (((int)*(double *)(array->data + ARRAY_STATE*array->strides[0] + i*array->strides[1])) >= (ARRAY_STATE_DANG + nb_players - 1)) 
-				      	(*(double *)(array->data + ARRAY_STATE*array->strides[0] + i*array->strides[1])) = (double)(ARRAY_STATE_DANG);
-						else
-				      	(*(double *)(array->data + ARRAY_STATE*array->strides[0] + i*array->strides[1]))++;
-							
-				      (*(double *)(array->data + ARRAY_UNTIL*array->strides[0] + i*array->strides[1])) = min(t_x,  (*(double *)(array->data + ARRAY_UNTIL*array->strides[0] + i*array->strides[1])) );
-			    }
+				else if (dabs(b_y - y) > RADIUS)
+				{
+					signe_y = (b_y > y) + (b_y >= y) - 1;
+					rat_y = (-1)*COSINUS_LIST[truemod((int)(10*b_d),3600)]*b_s - signe_y * PLAYER_SPEED;
+					if (rat_y != 0)
+					{
+						t_y = (signe_y * RADIUS - b_y + y) / rat_y;
+						if (t_y >= 0)
+						{
+							if ((int)array_elem(STATE) >= ARRAY_STATE_DANG + nb_players - 1)
+								array_elem(STATE) = (double)(ARRAY_STATE_DANG);
+							else
+								array_elem(STATE)++;
+
+							array_elem(UNTIL) = min(t_y, array_elem(UNTIL));
+						}
+					}
+				}
+				else
+					result=i; 
 			}
-		    }
 		}
-	      else if (dabs(b_y - y) > RADIUS)
-		{
-		  signe_y = (b_y > y) + (b_y >= y) - 1;
-		  rat_y = (-1)*COSINUS_LIST[truemod((int)(10*b_d),3600)]*b_s - signe_y * PLAYER_SPEED;
-		  if (rat_y != 0)
-		    {
-		      t_y = (signe_y * RADIUS - b_y + y) / rat_y;
-		      if (t_y >= 0)
-			{
-					 	if (((int)*(double *)(array->data + ARRAY_STATE*array->strides[0] + i*array->strides[1])) >= (ARRAY_STATE_DANG + nb_players - 1)) 
-				      	(*(double *)(array->data + ARRAY_STATE*array->strides[0] + i*array->strides[1])) = (double)(ARRAY_STATE_DANG);
-						else
-				      	(*(double *)(array->data + ARRAY_STATE*array->strides[0] + i*array->strides[1]))++;
-							
-				      (*(double *)(array->data + ARRAY_UNTIL*array->strides[0] + i*array->strides[1])) = min(t_y,  (*(double *)(array->data + ARRAY_UNTIL*array->strides[0] + i*array->strides[1])) );
-			}
-		    }
-		}
-	      else
-				result=i; 
-}	    
-}	
-}    
-  return (Py_BuildValue("i",result));
+	}
+	return (Py_BuildValue("i",result));
 }
 
 static PyMethodDef DrawMethods[] = {
@@ -156,12 +166,12 @@ static PyMethodDef DrawMethods[] = {
 
 PyMODINIT_FUNC initcoll(void)
 {
-  int i;
-  (void) Py_InitModule("coll", DrawMethods);
-  import_array();
-  for (i=0;i<3600;i++)
-    {
-      SINUS_LIST[i] = sin(((double)i*M_PI)/(double)1800);
-      COSINUS_LIST[i] = cos(((double)i*M_PI)/(double)1800);
-    }
+	int i;
+	(void) Py_InitModule("coll", DrawMethods);
+	import_array();
+	for (i=0;i<3600;i++)
+	{
+		SINUS_LIST[i] = sin(((double)i*M_PI)/(double)1800);
+		COSINUS_LIST[i] = cos(((double)i*M_PI)/(double)1800);
+	}
 }
--- draw.c
+++ draw.c
@@ -6,6 +6,8 @@
 #define ARRAY_Z 2
 #define ARRAY_LIST 5
 
+#define array_elem(type) *(double *)(array->data + ARRAY_##type*array->strides[0] + j*array->strides[1])
+
 static PyObject *draw(PyObject *self, PyObject *args)
 {
 	int card,j;
@@ -16,8 +18,8 @@ static PyObject *draw(PyObject *self, PyObject *args)
 	  {
 	    glPushMatrix();
 	    glColor4f(1.0, 1.0, 1.0, 0.2);
-	    glTranslatef((*(double *)(array->data + ARRAY_X*array->strides[0] + j*array->strides[1])),(*(double *)(array->data + ARRAY_Y*array->strides[0] + j*array->strides[1])), (*(double *)(array->data + ARRAY_Z*array->strides[0] + j*array->strides[1])));
-	    glCallList((int)*(double*)(array->data + ARRAY_LIST*array->strides[0] + j*array->strides[1]));
+	    glTranslatef(array_elem(X),array_elem(Y), array_elem(Z));
+	    glCallList((int) array_elem(LIST));
 	    glPopMatrix();
 	  }
 	
